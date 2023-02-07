807640dc2eaaea158da5f2051343d1cb1b08ff69
Henrique Correa do Amaral <henrique.amaral@rhitmo.com.br>
2023-02-07 17:00:43 -0300

    feat(user): criada entidade de usuario

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UsersDocument = IUser & Document;
+
+@Schema()
+export class IUser {
+    @Prop({ required: true, index: true })
+    name: string;
+
+    @Prop({ required: true, unique: true, index: true })
+    email: string;
+
+    @Prop({ required: true, select: false, index: true })
+    password: string[];
+
+}
+
+export const ClassesSchema = SchemaFactory.createForClass(IUser).set(
+    'timestamps',
+    true,
+);
