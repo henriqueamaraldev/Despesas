90d981bbb21e8bcadba8b2d2aa7df2f2bc8e805b
Henrique Correa do Amaral <henrique.amaral@rhitmo.com.br>
2023-02-08 00:49:22 -0300

    feat(validations): value has to be positive and description max length is now 191

--- src/expenses/dto/create-expenses.dto.ts
+++ src/expenses/dto/create-expenses.dto.ts
@@ -1,17 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNotEmpty, IsString, IsDate, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsString, IsDate, IsNumber, MaxLength, MaxDate, IsPositive } from 'class-validator';
 
 export class CreateExpensesDto {
 
     @IsString()
+    @MaxLength(191)
     description: string;
 
 
     @IsDate()
+    @MaxDate(new Date())
     date: string;
 
 
     @IsNumber()
+    @IsPositive()
     @IsNotEmpty()
     value: string;
 
