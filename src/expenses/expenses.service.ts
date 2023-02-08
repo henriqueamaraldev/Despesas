1eb7a14b39e09c8035787be6665c0ce9150e3cbf
Henrique Correa do Amaral <henrique.amaral@rhitmo.com.br>
2023-02-08 00:51:22 -0300

    feat(expenses): expenses crude services created

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { ExpensesDocument, IExpenses } from './entities/expenses.entity';
-import { CreateExpensesDto } from './dto/create-expenses.dto';
+import { CreateExpensesDto, UpdateExpensesDto } from './dto/create-expenses.dto';
 
 @Injectable()
 export class ExpensesService {
@@ -12,4 +12,86 @@ export class ExpensesService {
         @InjectModel(IExpenses.name) private expensesModel: Model<ExpensesDocument>
     ) { }
 
+
+    async listExpenses(userId: string) {
+
+        try {
+
+            let userExpenses = await this.expensesModel.find({ userId, isActive: true });
+            return userExpenses;
+
+        } catch (error) {
+
+            console.log("Error trying to find user's expenses", error);
+
+        }
+    }
+
+    async getExpenseById(id: string) {
+
+        try {
+
+            let expense = await this.expensesModel.findById(id);
+
+            return expense;
+
+        } catch (error) {
+
+            console.log("Error trying to find expense", error);
+
+        }
+    }
+
+
+    async createExpense(data: CreateExpensesDto, userId: string) {
+
+        try {
+
+            let newExpense = await this.expensesModel.create({ ...data, userId: userId });
+
+            return newExpense;
+
+        } catch (error) {
+
+            console.log("Error trying to create expense", error);
+
+        }
+    }
+
+
+    async deleteExpense(expenseId: string, userId: string) {
+
+        try {
+
+            await this.expensesModel.findOneAndUpdate(
+                { _id: expenseId, userId: userId },
+                { isActive: false }
+            );
+
+        } catch (error) {
+
+            console.log("Error trying to delete expense", error);
+
+        }
+    }
+
+
+    async updateExpense(expenseId: string, userId: string, data: UpdateExpensesDto) {
+
+        try {
+
+            let updatedExpense = await this.expensesModel.findOneAndUpdate(
+                { _id: expenseId, userId: userId },
+                data
+            );
+
+            return updatedExpense;
+
+        } catch (error) {
+
+            console.log("Error trying to update expense", error);
+
+        }
+
+    }
 }
